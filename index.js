5e12c67ce36e621dff7082fb952503a9b38811b2
andreamabel <andreamabel972@gmail.com>
2023-01-09 22:13:13 +0200

    Online API Java

---
+++ index.js
@@ -0,0 +1,23 @@
+// const html = document.querySelector('html')
+
+// html.addEventListener('mouseover', (e)=>{
+//     e.preventDefault()
+//     let cont = 5;
+//     const result = document.querySelector('.result')
+//     const int = setInterval(() => {
+//         if(cont != 0){
+//             result.innerHTML = `Redirigiendo en ${cont - 1}`
+//         }else{
+//             clearInterval(int)
+//             window.location.href = "https://asdsadsdasda.com"
+//         }
+//     }, 500);
+// })
+
+
+// const a = document.querySelector('html')
+// a.addEventListener('mouseover', ()=>{
+//     setTimeout(()=>{window.location.href = "https://backend-andrea-ap.onrender.com"}, 500)
+//    console.log("HTML")
+// })
+
